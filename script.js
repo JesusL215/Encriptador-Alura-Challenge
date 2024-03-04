d42ae0f72266ac4dff6fb58f33cd352491d437cb
Jesus Loyola <jesus.jli215@gmail.com>
2024-03-04 18:58:07 -0500

    Subiendo archivos al repositorio

---
+++ script.js
@@ -0,0 +1,112 @@
+const encriptar = document.getElementById("button_encriptar");
+const desencriptar = document.getElementById("button_desencriptar");
+const copy = document.getElementById("button_copiar");
+const textoInicial = document.getElementById("texto-inicial");
+const textFinal = document.getElementById("textoFinal");
+const munheco = document.getElementById("munheco");
+const textInfo = document.getElementById("textoInfo");
+const rigth = document.getElementById("salida")
+
+textoInicial.addEventListener('input', function() {
+    let texto = this.value.toLowerCase();
+    // Eliminar acentos
+    texto = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    // Eliminar caracteres especiales acentuados
+    texto = texto.replace(/[áäâàåãéëêèíïîìóöôòøõúüûù]/g, function(match) {
+        return match.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    });
+    this.value = texto;
+});
+
+const remplace = (newvalue) => {
+	textFinal.innerHTML = newvalue;
+	textFinal.classList.add("ajustar");
+	rigth.classList.add("ajuste")
+	textoInicial.value = "";
+	textoInicial.style.height = "auto"
+	textoInicial.placeholder = "Ingrese el texto aquí";
+	munheco.classList.add("ocultar");
+	textInfo.classList.add("ocultar");
+	copy.classList.remove("bn_ocultar");
+}
+
+const reset = () => {
+	textoInicial.value = "";
+    textoInicial.style.height = "auto";
+	textFinal.innerHTML = "";
+	rigth.classList.remove("ajuste")
+	textFinal.classList.remove("ajustar");
+	munheco.classList.remove("ocultar");
+	textFinal.placeholder = "Ningún mensaje fue encontrado";
+	textInfo.classList.remove("ocultar")
+	copy.classList.add("bn_ocultar");
+	textoInicial.focus();
+};
+
+let remplazar = [
+	["e", "enter"],
+	["o", "ober"],
+	["i", "imes"],
+	["a", "ai"],
+	["u", "ufat"]
+];
+
+encriptar.addEventListener('click', () => {
+
+	const texto = textoInicial.value.toLowerCase();
+
+	if (texto != "") {
+		function encript(newtext) {
+			for (let i = 0; i < remplazar.length; i++) {
+				if (newtext.includes(remplazar[i][0])) {
+					newtext = newtext.replaceAll(remplazar[i][0], remplazar[i][1]);
+				};
+			};
+			return newtext;
+		};
+		remplace(encript(texto));
+	} else {
+		alert("Ingrese texto para encriptar");
+		reset();
+	};
+});
+
+desencriptar.addEventListener('click', () => {
+
+	const texto = textoInicial.value.toLowerCase();
+
+	if (texto != "") {
+		function desencript(newtext) {
+			for (let i = 0; i < remplazar.length; i++) {
+				if (newtext.includes(remplazar[i][1])) {
+					newtext = newtext.replaceAll(remplazar[i][1], remplazar[i][0]);
+				};
+			};
+			return newtext;
+		};
+		remplace(desencript(texto));
+	} else {
+		alert("Ingrese texto a desencriptar");
+		reset();
+	};
+});
+
+copy.addEventListener("click", () => {
+	let texto = textFinal;
+	texto.select();
+	document.execCommand('copy');
+	alert("Texto Copiado");
+	reset();
+});
+
+//auto ajuste de textarea
+textoInicial.addEventListener("change", e => {
+	textoInicial.style.height = "auto";
+	let scHeight = e.target.scrollHeight;
+	textoInicial.style.height = `${scHeight}px`;
+});
+textoInicial.addEventListener("keyup", e => {
+	textoInicial.style.height = "auto";
+	let scHeight = e.target.scrollHeight;
+	textoInicial.style.height = `${scHeight}px`;
+});
